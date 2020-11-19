cbf3b97361d60c80e3259ab6ad86d6725d81cfcc
victor bonjean <vbonjean@cluster-r740-volta-1.ias.u-psud.fr>
2020-11-19 09:54:16 +0100

    fix bug

--- mnist_test.py
+++ mnist_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import keras
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from keras.datasets import mnist
@@ -11,18 +11,28 @@ from tqdm import tqdm
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+x_train = x_train.reshape((-1, 1, 28, 28)).astype('float32')
+x_test = x_test.reshape((-1, 1, 28, 28)).astype('float32')
 x_train /= 255
 x_test /= 255
 
-y_train_cnn = keras.utils.to_categorical(y_train, 10)
-y_test_cnn = keras.utils.to_categorical(y_test, 10)
-x_train_cnn = np.expand_dims(x_train, -1)
-x_test_cnn = np.expand_dims(x_test, -1)
+#x_train_cnn = np.expand_dims(x_train, -1)
+#x_test_cnn = np.expand_dims(x_test, -1)
 
-data_train_loader = DataLoader((x_train_cnn, y_train_cnn), batch_size=256, shuffle=True, num_workers=8)
-data_test_loader = DataLoader((x_test_cnn, y_test_cnn), batch_size=1024, num_workers=8)
+class TMPDataset(Dataset):
+
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
+
+    def __getitem__(self, item):
+        return self.x[item], self.y[item]
+
+    def __len__(self):
+        return len(self.y)
+
+data_train_loader = DataLoader(TMPDataset(x_train, y_train), batch_size=256, shuffle=True, num_workers=8)
+data_test_loader = DataLoader(TMPDataset(x_test, y_test), batch_size=1024, num_workers=8)
 
 net = LeNet5().type(torch.cuda.FloatTensor)
 if torch.cuda.device_count() > 1:

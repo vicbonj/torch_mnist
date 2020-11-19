80f51f1aa13ff3b1640cd940898d3e44e8554c8e
victor bonjean <vbonjean@cluster-r740-volta-1.ias.u-psud.fr>
2020-11-19 10:29:37 +0100

    bug

--- net/networks.py
+++ net/networks.py
@@ -6,26 +6,23 @@ class LeNet5(nn.Module):
     def __init__(self):
         super(LeNet5, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 6, 5, padding=1, padding_mode='reflection'))
+        self.conv1 = nn.Conv2d(1, 6, 5)#, padding=1, padding_mode='reflection')
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(2, stride=2)
-        self.conv2 = nn.Conv2d(6, 16, 5, padding=1, padding_mode='reflection'))
-        self.conv3 = nn.Conv2d(16, 120, 5, padding=1, padding_mode='reflection'))
+        self.conv2 = nn.Conv2d(6, 16, 5, padding=1, padding_mode='reflection')
+        self.conv3 = nn.Conv2d(16, 120, 5, padding=1, padding_mode='reflection')
         self.dense1 = nn.Linear(120, 84)
         self.dense2 = nn.Linear(84, 10)
         self.last_act = nn.LogSoftmax(dim=-1)
         
     def forward(self, x):
-        print(x.size())
+        print(x.size(0))
         output = self.maxpool(self.relu(self.conv1(x)))
-        print(output.size())
-        x = self.maxpool(self.relu(self.conv2(output)))
-        print(x.size())
+        y = self.maxpool(self.relu(self.conv2(output)))
         output = self.maxpool(self.relu(self.conv2(output)))
-        print(output.size())
-        output += x
+        output += y
         output = self.relu(self.conv3(output))
-        output = output.view(img.size(0), -1)
+        output = output.view(x.size(0), -1)
         output = self.relu(self.dense1(output))
         output = self.last_act(self.dense2(output))
         return output
